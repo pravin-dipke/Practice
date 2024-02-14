baef57036c06c14e81432213a055fd0a5ce16d39
Pravin Dipke <pravinbdipke@gmail.com>
2024-02-14 19:10:36 +0530

    java files modified

---
+++ Java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        char myVar1 = 65;
+        char myVar2 = 87;
+        System.out.println("myVar1:- " + myVar1);
+        System.out.println("myVar2:- " + myVar2);
+
+    }
+}
